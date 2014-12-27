a6dd7ad5528eddc01924ce405856b2087c5d21e1
Daniel Barella <dan.barella@gmail.com>
2014-12-27 16:59:08 -0200

    Added util.py (utility functions)

---
+++ util.py
@@ -0,0 +1,14 @@
+"""Utility functions."""
+
+
+def pad(field_2d, padding):
+  """Returns a version of field_2d padded on all sides with padding.
+
+  Args:
+    field_2d (list of (list of object)): A 2-dimensional field to pad.
+    padding (object): The padding with which to surround field_2d.
+
+  Returns (list of (list of object)): A padded 2-dimensional field.
+  """
+  # max_width = max(map(len, field_2d))
+  pass
