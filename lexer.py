f5191c36397b5614c8fde7c844809bb30222fa59
Daniel Barella <dan.barella@gmail.com>
2014-12-27 17:54:39 -0200

    On the road to 2D lexing.
    Implemented basic directional stuff.

--- lexer.py
+++ lexer.py
@@ -1,8 +1,7 @@
-# KitKat Parser
+"""KitKat lexer."""
 
 
 from __future__ import print_function
-import itertools
 import sys
 
 import token
@@ -13,7 +12,17 @@ __author__ = 'dan.barella@gmail.com (Dan Barella)'
 
 
 class Lexer(object):
-  """Lexes KitKat token according to the rules described in the README."""
+  """Lexes KitKat token according to the rules described in the README.
+
+  Internally, the lexer defines the first character in the file as the logical
+  position (0, 0), and the last character in the file as (len, len).
+  """
+
+  # Read direction
+  UP = 0
+  RIGHT = 1
+  DOWN = 2
+  LEFT = 3
 
   def __init__(self, source):
     """Init a Lexer with a source file.
@@ -27,7 +36,9 @@ class Lexer(object):
     """
     self._source = source
     self.lines = source.readlines()
-    self.text_field = util.pad(self.lines, None)
+    self.text_field = self.lines  # util.pad(self.lines, None)
+
+    self._direction = Lexer.RIGHT
 
   def __iter__(self):
     """Return a token iterator over the input text.
@@ -41,22 +52,44 @@ class Lexer(object):
     """
 
     def _token_generator():
-      for line in self.source:
+      import pdb; pdb.set_trace()
+      # Current location in self.text_field
+      # i.e. self.text_field[i][j]
+      i = 0
+      j = 0
+
+      while i < len(self.text_field) and j < len(self.text_field[i]):
+        ch = self.text_field[i][j]  # Current character
+
+        # TODO(barella): Chech for control character, change direction.
 
         # Since KitKat needs a 1-char lookahead for escapes,
         # we iterate over character pairs
-        pairs = itertools.izip_longest(line, line[1:])
 
-        for ch1, ch2 in pairs:
-          t = token.Token(ch1, ch2)
+        # for ch1, ch2 in pairs:
+        #   t = token.Token(ch1, ch2)
 
-          # If we get an escaped character, skip the lookahead.
-          # Note that characterize takes care of improperly escaped characters.
-          if not t.is_kind(token.TOKENS[...]):
-            yield t
-            continue
-          else:
-            yield t
+        #   # If we get an escaped character, skip the lookahead.
+        #   # Note that characterize takes care of improperly escaped characters.
+        #   if not t.is_kind(token.TOKENS[...]):
+        #     yield t
+        #     continue
+        #   else:
+        #     yield t
+        yield ch
+
+        if self._direction == Lexer.UP:
+          i -= 1
+        elif self._direction == Lexer.DOWN:
+          i += 1
+        elif self._direction == Lexer.RIGHT:
+          j += 1
+        elif self._direction == Lexer.LEFT:
+          j -= 1
+        else:  # Invalid state
+          raise LexerException(
+              'Something dinked. {} is not a valid lexer direction.'.format(
+                self._direction))
 
     return _token_generator()
 
