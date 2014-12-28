70ea816d6561d8586ad8adae1a6a7043801afe98
Daniel Barella <dan.barella@gmail.com>
2014-12-28 01:40:07 -0200

    Removed dead code.

--- kk_token_test.py
+++ kk_token_test.py
@@ -34,26 +34,5 @@ class TestToken(unittest.TestCase):
                     and down.is_directional())
 
 
-# class TestKind(unittest.TestCase):
-#
-#   def test_non_special_char(self):
-#     """Test an unspecial character."""
-#     actual_kind = token.Token.t_kind('a', None)
-#
-#     self.assertEqual(token.TOKENS[...], actual_kind)
-#
-#   def test_escaped_right(self):
-#     """Test an escaped > character."""
-#     actual_kind = token.Token.t_kind('\'', '>')
-#
-#     self.assertEqual(token.TOKENS[...], actual_kind)
-#
-#   def test_escaped_newline(self):
-#     """Test an escaped newline."""
-#     actual_kind = token.Token.t_kind('\'', 'n')
-#
-#     self.assertEqual(token.TOKENS[...], actual_kind)
-
-
 if __name__ == '__main__':
   unittest.main()
